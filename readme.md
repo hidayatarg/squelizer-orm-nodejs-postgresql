e1ee4e009aedf5d13b4467fc5578a9866bc638dd
Hidayat Arghandabi <hidayatarg@gmail.com>
2021-03-16 01:59:59 +0300

    Updated Database To Docker

--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 - Create Generic routes
 - Added Middlewares
+
+## Dockerization
+
+- Download docker image `docker pull postgres`
+- Run docker image `docker run --name docker-postgres -e POSTGRES_PASSWORD=postgres -d postgres`
+
+Username and password are set to postgres
--- src/config/env.js
+++ src/config/env.js
@@ -1,16 +1,15 @@
 const env = {
-    database: 'localhost',
-    username: 'postgres',
-    password: '123',
-    host: 'localhost',
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+  database: 'postgres',
+  username: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 };
 
-  
 module.exports = env;
